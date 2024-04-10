353af71ff44c82ec9976b8e25f511aa65fa26e36
ChanceCtrl <StaticElite05@gmail.com>
2024-04-10 15:43:20 +0000

    week12

---
+++ week12/README.md
@@ -0,0 +1,1 @@
+simple guy, and as time goes on my variable names get more and more dereanged
---
+++ week12/main.py
@@ -0,0 +1,35 @@
+import os
+import sense_hat
+
+from time import sleep
+
+if __name__ == "__main__":
+    cont = True
+    th = 2
+    sh = sense_hat.SenseHat()
+
+    # Compass, Gyro, Accelerometer
+    sh.set_imu_config(False, False, True)
+
+    while cont:
+        acl = sh.get_accelerometer_raw()
+
+        for event in sh.stick.get_events():
+            if event.action == "held":
+                sh.clear()  # Display NONE
+                sleep(1)
+                cont = False
+
+        # "sensehat accelerating" state
+        if abs(acl["x"]) >= th or abs(acl["y"]) >= th or abs(acl["z"]) >= th:
+            print("Was shaken")
+            stream = os.popen("ping -c 3 google.com")
+            o = stream.readlines()
+            joe_split = o[7].split(" ")
+            split_joe_split = joe_split[3].split("/")
+            print("joe ping: ", split_joe_split[1])
+            sh.show_message((split_joe_split[1], " ms"), 1.0, [0, 255, 100])
+
+        # "sensehat stationary" state
+        else:
+            sh.clear()  # Display NONE
